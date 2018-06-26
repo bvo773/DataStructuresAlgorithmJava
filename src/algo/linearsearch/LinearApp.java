1d6f11c3c54f0b7757e6577f217fee888420d1f0
iMTrashbin <binhvo07@gmail.com>
2018-06-26 14:26:34 -0500

    Linear search, ez

---
+++ src/algo/linearsearch/LinearApp.java
@@ -0,0 +1,18 @@
+package algo.linearsearch;
+
+
+
+public class LinearApp {
+
+    public static int linearSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key)
+                return arr[i];
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(LinearApp.linearSearch(new int[] {12,3,4}, 3));
+    }
+}
