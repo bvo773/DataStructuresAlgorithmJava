8a948732483f9a1640aba02498268b4bfeddce7e
Binh Vo <binhvo@Binhs-MBP.hsd1.il.comcast.net>
2018-07-13 15:33:24 -0500

    Added psedocude for merge sort

--- src/algo/mergesort/MergeSortApp.java
+++ src/algo/mergesort/MergeSortApp.java
@@ -4,10 +4,15 @@ public class MergeSortApp {
 // Two major steps in mergeSort
 // 1. "Splitting" the array into left and right subarrays mergeSort(), invoking itself until it hits the base(1 slot)
 // 2. "Sorting" takes during merge(). Before merging(), both arrays MUST BE SORTED. If they are not sorted, we can't call merge;
-
-//    public static void sort(int[] array, int start , int end) {
-//        if (end <=)
-//    }
+/*  mergeSort(A, p, r) {                p = left index  q = middle index r = right index
+        if (p < r) {
+            q = [(p+r)/2]
+            mergeSort(A,p,q)
+            mergeSort(A, q + 1, r)
+            merge(A, p, q, r)
+    }
+*/
+//
 //
     public static void sort(int i, int n) {
         if (i < n) {
