99b6fbea9b7aadf20265fe4e107b687e4356885d
bvo773 <binhvo07@gmail.com>
2020-12-12 18:09:44 -0600

    added my binary search

--- src/algo/binarysearch/BinarySearchApp.java
+++ src/algo/binarysearch/BinarySearchApp.java
@@ -1,6 +1,5 @@
 package algo.binarysearch;
 
-
 public class BinarySearchApp {
     public static int binarySearch(int x, int[] arr) {
         int p = 0; // First(Left) index position of the range
@@ -22,9 +21,24 @@ public class BinarySearchApp {
         return -1;
     }
 
+    public static int binhBinarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (array[mid] == target) { return mid;}
+            else if (array[mid] > target) { right = mid - 1; }
+            else if (array[mid] < target) { left = mid + 1; }
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
         //int[] arr = new int[] {3, 11, 21, 29, 41, 54, 61, 78, 110, 127};
         //System.out.println(binarySearch(78, arr));
+        int[] arr = new int[] {1,2,3,4,5};
+        int target = 5;
+        System.out.println(binhBinarySearch(arr, target));
     }
 }
